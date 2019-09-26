91941b4ecbe748f4494087f565534a24c5de1dd5
Pat Haynes <pathaynes32@gmail.com>
2019-09-26 16:35:34 -0700

    one verticle slice down

---
+++ e2e/__tests__/reviewers.test.js
@@ -0,0 +1,39 @@
+const request = require('../request');
+const db = require('../db');
+
+describe('reviewer api', () => {
+  beforeEach(() => {
+    return db.dropCollection('reviewers');
+  });
+
+  const ebert = {
+    name: 'Roger Ebert',
+    company: 'At the Movies'
+  };
+
+  function postReviewer(dude) {
+    return request
+      .post('/api/reviewers')
+      .send(dude)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('post a dude', () => {
+    return postReviewer(ebert).then(dude => {
+      expect(dude).toMatchInlineSnapshot(
+        {
+          _id: expect.any(String)
+        },
+        `
+        Object {
+          "__v": 0,
+          "_id": Any<String>,
+          "company": "At the Movies",
+          "name": "Roger Ebert",
+        }
+      `
+      );
+    });
+  });
+});
