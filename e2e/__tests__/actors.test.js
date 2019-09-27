7c3eaeab912fcc2c4df4cf93335018d1ba88c659
Dave Trost <davetrost@gmail.com>
2019-09-27 15:19:55 -0700

    get actor by id tested

--- e2e/__tests__/actors.test.js
+++ e2e/__tests__/actors.test.js
@@ -39,22 +39,26 @@ describe('actor api', () => {
     });
   });
 
-  it.skip('gets a dude by id', () => {
+  it('gets an actor by id', () => {
     return postActor(ed).then(dude => {
       return request
-        .post('/api/reviews')
-        .send({
-          rating: 5,
-          review: `"Fight Club" A celebration of violence in which the heroes write themselves a license to drink, smoke, screw and beat one another up.`,
-          film: 0,
-          actor: dude._id
-        })
+        .get(`/api/actors/${dude._id}`)
         .expect(200)
-        .then(() => {
-          return request.get(`/api/actors/${dude._id}`).expect(200);
-        })
         .then(({ body }) => {
-          return expect(body).toMatchInlineSnapshot();
+          expect(body).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "dob": "1969-08-18T07:00:00.000Z",
+              "name": "Edward Norton",
+              "pob": "Boston, Massachusetts",
+            }
+          `
+          );
         });
     });
   });
