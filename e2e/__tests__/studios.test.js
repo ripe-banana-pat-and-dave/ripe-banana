ad2bb1538e5ce136e0b04bfd32afaabd2ccecb45
Pat Haynes <pathaynes32@gmail.com>
2019-09-27 15:51:39 -0700

    fuck ya

---
+++ e2e/__tests__/studios.test.js
@@ -0,0 +1,100 @@
+const request = require('../request');
+const db = require('../db');
+
+describe('studio api', () => {
+  beforeEach(() => {
+    return db.dropCollection('studios');
+  });
+
+  const house = {
+    name: 'Warner Bros. Studio',
+    address: {
+      city: 'Los Angeles',
+      state: 'CA',
+      country: 'USA'
+    }
+  };
+
+  function postStudio(house) {
+    return request
+      .post('/api/studios')
+      .send(house)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('post a house', () => {
+    return postStudio(house).then(house => {
+      expect(house).toMatchInlineSnapshot(
+        {
+          _id: expect.any(String)
+        },
+        `
+        Object {
+          "__v": 0,
+          "_id": Any<String>,
+          "address": Object {
+            "city": "Los Angeles",
+            "country": "USA",
+            "state": "CA",
+          },
+          "name": "Warner Bros. Studio",
+        }
+      `
+      );
+    });
+  });
+
+  it('gets a list of studios', () => {
+    return Promise.all([
+      postStudio(house),
+      postStudio(house),
+      postStudio(house),
+      postStudio(house)
+    ])
+      .then(() => {
+        return request.get('/api/studios').expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(4);
+        expect(body[0]).toMatchInlineSnapshot(
+          {
+            _id: expect.any(String)
+          },
+          `
+          Object {
+            "_id": Any<String>,
+            "name": "Warner Bros. Studio",
+          }
+        `
+        );
+      });
+  });
+
+  it('gets an studio by id', () => {
+    return postStudio(house).then(house => {
+      return request
+        .get(`/api/studios/${house._id}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "address": Object {
+                "city": "Los Angeles",
+                "country": "USA",
+                "state": "CA",
+              },
+              "name": "Warner Bros. Studio",
+            }
+          `
+          );
+        });
+    });
+  });
+});
